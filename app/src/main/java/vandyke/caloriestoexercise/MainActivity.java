25a224f4079b8a4c224901a2efbcb2ff4fabe5f3
NickvanDyke <nickjvandyke15@gmail.com>
2017-05-16 22:41:11 -0500

    Properly add ActionBar and Settings activity

--- app/src/main/java/vandyke/caloriestoexercise/MainActivity.java
+++ app/src/main/java/vandyke/caloriestoexercise/MainActivity.java
@@ -3,6 +3,7 @@ package vandyke.caloriestoexercise;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.*;
@@ -21,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set the toolbar
+        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
 
         // create and add BaseBurnActivity
         burnActivities = new ArrayList<>();
@@ -79,9 +83,5 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_actionbar, menu);
         return true;
-    }
-
-    public void openSettings(View view) {
-
     }
 }
--- app/src/main/java/vandyke/caloriestoexercise/SettingsActivity.java
+++ app/src/main/java/vandyke/caloriestoexercise/SettingsActivity.java
@@ -1,7 +1,11 @@
 package vandyke.caloriestoexercise;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import static vandyke.caloriestoexercise.SettingsActivity.UNITS.IMPERIAL;
 
@@ -15,5 +19,27 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+
+        // enable up arrow
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish(); // finish this activity, returning to the previous one
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.settings_actionbar, menu);
+        return true;
     }
 }
